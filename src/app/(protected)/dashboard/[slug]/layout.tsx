924f7df0b51640ee1a3ef874b94d17d359fa6007
20hamza01 <90298569+20hamza01@users.noreply.github.com>
2024-12-07 20:03:16 +0100

    Sidebar and Navbar Done

--- src/app/(protected)/dashboard/[slug]/layout.tsx
+++ src/app/(protected)/dashboard/[slug]/layout.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Sidebar from '@/components/global/sidebar'
+import Navbar from '@/components/global/navbar'
 
 type Props = {
     children:React.ReactNode
@@ -8,10 +10,24 @@ type Props = {
 const Layout = ({children,params}: Props) => {
 
     // Query Client
-
+    // WIP: Query client fetch data
 
   return (
-    <div>Layout</div>
+    <div className='p-3'>
+        {/* Sidebar */}
+        <Sidebar slug={params.slug}/>
+        {/* Naviagtion */}
+        <div className='
+          lg:ml-[250px] 
+          lg:pl-10 
+          lg:py-5 
+          flex 
+          flex-col 
+          overflow-auto
+        '>
+            <Navbar slug={params.slug}/>
+        </div>
+    </div>
   )
 }
 
