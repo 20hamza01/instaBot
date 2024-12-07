4b902b1e27d8f7d75294b54d94ccb31dde2656e6
20hamza01 <90298569+20hamza01@users.noreply.github.com>
2024-12-07 17:33:18 +0100

    Clerk and Prisma setup

---
+++ src/app/(protected)/dashboard/[slug]/layout.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+type Props = {
+    children:React.ReactNode
+    params:{slug:string}
+}
+
+const Layout = ({children,params}: Props) => {
+
+    // Query Client
+
+
+  return (
+    <div>Layout</div>
+  )
+}
+
+export default Layout
---
+++ src/app/(protected)/dashboard/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+type Props = {}
+
+const Page = (props: Props) => {
+
+    // Work In Progress (WIP): Server Action Onboard thr user
+    // 200 || 201
+
+
+  return (
+    <div>Page</div>
+  )
+}
+
+export default Page
