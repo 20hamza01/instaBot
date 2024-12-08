545b00b94e40805cb5b85c9217c86d1ba2d8e357
20hamza01 <90298569+20hamza01@users.noreply.github.com>
2024-12-08 03:12:21 +0100

    UI for Automations setup

---
+++ src/app/(protected)/dashboard/[slug]/automations/[id]/page.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import AutomationsBreadCrumb from '@/components/global/bread-crumbs/automations'
+import { Warning } from '@/icons'
+import Trigger from '@/components/global/automations/trigger'
+
+type Props = {
+  params: { id: string }
+}
+
+/*
+export async function generateMetadata({ params }: { params: { id: string } }) {
+  const info = await getAutomationInfo(params.id)
+  return {
+    title: info.data?.name,
+  }
+}*/
+
+const Page = async ({ params }: Props) => {
+  //const query = new QueryClient()
+  //await PrefetchUserAutomation(query, params.id)
+
+  return (
+   
+      <div className=" flex flex-col items-center gap-y-20">
+        <AutomationsBreadCrumb id={params.id} />
+        <div className="w-full lg:w-10/12 xl:w-6/12 p-5 rounded-xl flex flex-col bg-[#1D1D1D] gap-y-3">
+          <div className="flex gap-x-2">
+            <Warning/>
+            When...
+          </div>
+          <Trigger id={params.id}/>
+        </div>
+      </div>
+    
+  )
+}
+
+export default Page
